37802d8fcdfdf3b848f5dd204e952c9dd3279d89
Omer Sahin <bilgi@omersahin.com.tr>
2025-01-24 19:26:44 +0300

    label parameters  updated

--- tkInterfaceLabel.py
+++ tkInterfaceLabel.py
@@ -7,7 +7,7 @@ window.minsize(width=600,height=600) # window minimum size
 #label:
 infoLabel = tk.Label(
     text="First Label:")
-infoLabel.config(bg="yellow", fg="blue") #Label properties
+infoLabel.config(bg="yellow", fg="blue",font=("Arial",30,"bold")) #Label properties
 infoLabel.pack() #place location
 
 window.mainloop() ##hold screen
