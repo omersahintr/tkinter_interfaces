04d4b9003c618674def10b84a54207478a2cf4e2
Omer Sahin <bilgi@omersahin.com.tr>
2025-01-25 22:19:00 +0300

    text object added on widgets file

--- tkInterfaceWidgets.py
+++ tkInterfaceWidgets.py
@@ -5,17 +5,32 @@ screen = Tk() # screen name of Tkinter object created
 screen.title("Widget Samples") #Screen title defined
 screen.minsize(width=300, height=300) #screen size defined
 
+def txt_read(get_txt):
+    label.config(text=get_txt)
+
+
+#Label:
 label = Label() #Label object created
 label.config(bg="yellow") #label backcolor set yellow
 label.config(fg="black") #label foreground color set black
 label.config(padx=20,pady=20) #label position pad-x and pad-y
 
+#Button:
 button = Button() #new button object created
 button.config(text="Resize Form", command=lambda: label.config(text="Try and Catch")) #button click action defined
 button.config(bg="yellow") #button backcolor set yellow
 button.config(fg="red") #button foreground color set red
 
+#Button2:
+btn_text = Button(text="Text Read", command=lambda: txt_read(txt.get("1.0",END))) #get:row1 to END. get:"2.0" to END.
+
+#Text object:
+txt = Text(width=30,height=10)
+
+
 button.pack() #show button
+btn_text.pack() #show btn_text
 label.pack() #show label
+txt.pack() #show text
 
 screen.mainloop()
