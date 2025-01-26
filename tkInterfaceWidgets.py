473e8960ab32e67d6be63000243620ba31171d7a
Omer Sahin <bilgi@omersahin.com.tr>
2025-01-26 13:01:34 +0300

    radiobutton and listbox objects added

--- tkInterfaceWidgets.py
+++ tkInterfaceWidgets.py
@@ -3,7 +3,7 @@ from tkinter import * #different import process
 
 screen = Tk() # screen name of Tkinter object created
 screen.title("Widget Samples") #Screen title defined
-screen.minsize(width=300, height=300) #screen size defined
+screen.minsize(width=300, height=400) #screen size defined
 
 def txt_read(get_txt):
     label.config(text=get_txt)
@@ -16,6 +16,12 @@ def select_spin():
 
 def select_check():
     label.config(text=IntVar().get())
+
+def select_radio():
+    label.config(text=radio_state.get())
+
+def select_list(event):
+    label.config(text=list1.get(list1.curselection()))
 
 #Label:
 label = Label() #Label object created
@@ -44,14 +50,29 @@ spin = Spinbox(from_=0,to=50, command=select_spin)
 #Checkbuttom:
 check = Checkbutton(text="option-1", variable=IntVar(), command=select_check)
 
+#Radio Buttons:
+radio_state = IntVar()
+radio1 = Radiobutton(text="option-1", value=1, variable=radio_state, command=select_radio)
+radio2 = Radiobutton(text="option-2", value=2, variable=radio_state, command=select_radio)
 
-button.pack() #show button
+#Listbox:
+list1 = Listbox()
+customer_list = ["Abraham","John","Emily","Sarah","Mack","Clara"]
+for i in range(len(customer_list)):
+    list1.insert(i,customer_list[i])
+list1.bind('<<ListboxSelect>>', select_list)
+
+
+"""button.pack() #show button
 btn_text.pack() #show btn_text
 txt.pack() #show text
 txt.focus() #txt object on focus
 level.pack()
 spin.pack()
-check.pack() ###
+check.pack()
+radio1.pack()
+radio2.pack()"""
+list1.pack()
 label.pack() #show label
 
 screen.mainloop()
